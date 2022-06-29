a2f31fb140f84b32cccce75969434aebd888c8f5
Laszlo Hasznos <hasznos.laszlo@gmail.com>
2022-06-29 16:02:17 +0200

    add tree structure algorithm

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useData from "../hooks/useData";
 
 const Navigation = () => {
@@ -5,7 +6,12 @@ const Navigation = () => {
 
   console.log({ data });
 
-  return <div>Navigation</div>;
+  return (
+    <div>
+      <div>Navigation</div>
+      {/* {data && data.map((item1) => <div>{item1.id}</div>)} */}
+    </div>
+  );
 };
 
 export default Navigation;
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export interface Data {
+export interface Item {
   BCAP1: string;
   BCAP2: string;
   BCAP3: string;
@@ -9,12 +9,96 @@ export interface Data {
   spend: number;
 }
 
+interface Tree {
+  [bcap1Id: string]: {
+    isOpen: boolean;
+    children: {
+      [bcap2Id: string]: {
+        isOpen: boolean;
+        children: {
+          [bcap3Id: string]: {
+            isOpen: boolean;
+            items: Item[];
+          };
+        };
+      };
+    };
+  };
+}
+
+const attachItemToBranch = (item: Item, tree: Tree) => {
+  const currentItems =
+    tree[item.BCAP1].children[item.BCAP2].children[item.BCAP3].items;
+  tree[item.BCAP1].children[item.BCAP2].children[item.BCAP3].items = [
+    ...currentItems,
+    item,
+  ];
+};
+
+const growBranch = (item: Item, tree: Tree) => {
+  const anyTree = tree as any;
+
+  if (anyTree[item.BCAP1]) {
+    if (anyTree[item.BCAP1].children[item.BCAP2]) {
+      if (anyTree[item.BCAP1].children[item.BCAP2].children[item.BCAP3]) {
+        // branch is ready, nothing to do
+      } else {
+        anyTree[item.BCAP1].children[item.BCAP2].children = {
+          [item.BCAP3]: {
+            isOpen: false,
+            items: [],
+          },
+        };
+      }
+    } else {
+      anyTree[item.BCAP1].children = {
+        [item.BCAP2]: {
+          isOpen: false,
+          children: {
+            [item.BCAP3]: {
+              isOpen: false,
+              items: [],
+            },
+          },
+        },
+      };
+    }
+  } else {
+    const anyTree = tree as any;
+    anyTree[item.BCAP1] = {
+      isOpen: false,
+      children: {
+        [item.BCAP2]: {
+          isOpen: false,
+          children: {
+            [item.BCAP3]: {
+              isOpen: false,
+              items: [],
+            },
+          },
+        },
+      },
+    };
+  }
+};
+
+const attachItemToTree = (item: Item, tree: Tree) => {
+  growBranch(item, tree);
+  attachItemToBranch(item, tree);
+};
+
 const useData = () => {
-  const [data, setData] = useState<Data | null>(null);
+  const [data, setData] = useState<Tree>({});
 
   const fetchData = async () => {
-    const data = await (await fetch("http://localhost:8080/data")).json();
-    setData(data);
+    const data: Item[] = await (
+      await fetch("http://localhost:8080/data")
+    ).json();
+    let tree: Tree = {};
+    data.forEach((item) => {
+      attachItemToTree(item, tree);
+    });
+    setData(tree);
   };
 
   useEffect(() => {
